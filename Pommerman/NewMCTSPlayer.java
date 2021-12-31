813c0812f610b5b4b6d2ff2e7b90d90ca6bc7f28
Jahnvi Sikligar <83291068+jahnvisikligar@users.noreply.github.com>
2021-12-31 00:15:59 +0000

    Add files via upload

---
+++ Pommerman/NewMCTSPlayer.java
@@ -0,0 +1,104 @@
+package GroupI.OpponentModel;
+
+import core.GameState;
+import players.Player;
+import players.mcts.MCTSParams;
+import players.optimisers.ParameterizedPlayer;
+import utils.ElapsedCpuTimer;
+import utils.Types;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class NewMCTSPlayer extends ParameterizedPlayer {
+
+    /**
+     * Random generator.
+     */
+    private Random m_rnd;
+
+    /**
+     * All actions available.
+     */
+    public Types.ACTIONS[] actions;
+
+    /**
+     * Params for this MCTS
+     */
+    public MCTSParams params;
+
+    public NewMCTSPlayer(long seed, int id) {
+        this(seed, id, new MCTSParams());
+    }
+
+    public NewMCTSPlayer(long seed, int id, MCTSParams params) {
+        super(seed, id, params);
+        reset(seed, id);
+
+        ArrayList<Types.ACTIONS> actionsList = Types.ACTIONS.all();
+        actions = new Types.ACTIONS[actionsList.size()];
+        int i = 0;
+        for (Types.ACTIONS act : actionsList) {
+            actions[i++] = act;
+        }
+    }
+
+    @Override
+    public void reset(long seed, int playerID) {
+        super.reset(seed, playerID);
+        m_rnd = new Random(seed);
+
+        this.params = (MCTSParams) getParameters();
+        if (this.params == null) {
+            this.params = new MCTSParams();
+            super.setParameters(this.params);
+        }
+    }
+
+    @Override
+    public Types.ACTIONS act(GameState gs) {
+
+        //Accessing the height and width of the board
+
+        if (gs.getBoard()[gs.getPosition().x][gs.getPosition().y].getKey() == 4){
+            return actions[0];
+        }
+        // TODO update gs
+        if (gs.getGameMode().equals(Types.GAME_MODE.TEAM_RADIO)){
+            int[] msg = gs.getMessage();
+        }
+
+        ElapsedCpuTimer ect = new ElapsedCpuTimer();
+        ect.setMaxTimeMillis(params.num_time);
+
+        // Number of actions available
+        int num_actions = actions.length;
+
+        // Root of the tree
+        NewSingleTreeNode m_root = new NewSingleTreeNode(params, m_rnd, num_actions, actions);
+        m_root.setRootGameState(gs);
+
+        //Determine the action using MCTS...
+        m_root.mctsSearch(ect);
+
+        //Determine the best action to take and return it.
+        int action = m_root.mostVisitedAction();
+        // TODO update message memory
+
+        //... and return it.
+        return actions[action];
+    }
+
+    @Override
+    public int[] getMessage() {
+        // default message
+        int[] message = new int[Types.MESSAGE_LENGTH];
+        message[0] = 1;
+        return message;
+    }
+
+    @Override
+    public Player copy() {
+        return new NewMCTSPlayer(seed, playerID, params);
+    }
+}
